e25e9fa9de08e0891ad193f7d379d0bece82c1b0
user <user@DESKTOP-SHR7SVO>
2022-01-13 21:53:24 +0530

    Add project files.

---
+++ Webapp/Data/WebappContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Webapp.Models;
+
+namespace Webapp.Data
+{
+    public class WebappContext : DbContext
+    {
+        public WebappContext (DbContextOptions<WebappContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Webapp.Models.Employee> Employee { get; set; }
+    }
+}
